383f7d32ab4d116f534d1f70d538d190cc2fe8cc
LaPetiteSouris <sontunghoang@gmail.com>
2017-08-23 16:54:01 +0200

    Use immutable filtering in categorical features of TrafficCluster

--- src/main/scala/TrafficCluster.scala
+++ src/main/scala/TrafficCluster.scala
@@ -10,16 +10,19 @@ object MainTrafficCluster {
 
     // Load the text into a Spark RDD, which is a distributed representation of each line of text
     val rawData = sc.textFile("hdfs:///user/ds/traffic.data")
-
+    println("Starting traffic")
     val data = rawData.map { line =>
       val lineSplit = line.split(",")
-      val lineFiltered = lineSplit.indices.collect { case i if i != 1 || i != 3 || i == line.length - 1 => lineSplit(i) }
+
+      // Temporarily remove string categorical features
+      // Immutable is better than mutable filtering
+      val lineFiltered = lineSplit.indices.collect { case i if !(i == 1 || i == 2 || i == 3 || i == (lineSplit.length - 1)) => lineSplit(i) }
       val label = lineSplit.last
       val values = lineFiltered.map(_.toDouble).toArray
+      // Feature vector
       val featureVector = Vectors.dense(values)
       (label, featureVector)
     }
-    print(data)
 
 
   }
