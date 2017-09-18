cf6854fdd990f94891f65075910441a4bd2c5843
LaPetiteSouris <sontunghoang@gmail.com>
2017-09-18 16:30:23 +0200

    Use bliki lib to parse Wikipedia XML

--- build.sbt
+++ build.sbt
@@ -8,9 +8,12 @@ libraryDependencies ++= {
   val sparkVer = "2.1.0"
   val sparkXMLVersion = "0.4.1"
   val cloud9Version = "2.0.1"
+  val blikiVer = "3.1.0"
   Seq(
     "org.apache.spark" %% "spark-core" % sparkVer % "provided" withSources(),
     "org.apache.spark" %% "spark-mllib" % sparkVer % "provided",
-    "com.databricks" %% "spark-xml" % sparkXMLVersion
+    "com.databricks" %% "spark-xml" % sparkXMLVersion,
+    "info.bliki.wiki" % "bliki-core" % blikiVer
+
   )
 }
--- src/main/scala/WikiAnalytics.scala
+++ src/main/scala/WikiAnalytics.scala
@@ -1,17 +1,50 @@
 package main.scala
 
+import java.io.ByteArrayInputStream
+
 import com.databricks.spark.xml.XmlInputFormat
 import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.spark.SparkContext
 import com.databricks.spark.xml.XmlInputFormat
+import info.bliki.wiki.dump.{IArticleFilter, Siteinfo, WikiArticle, WikiXMLParser}
+import org.apache.spark.rdd.RDD
+import org.xml.sax.SAXException
+
+
+case class Page(title: String, text: String, isCategory: Boolean, isFile: Boolean, isTemplate: Boolean)
+
+case class WrappedPage(var page: WikiArticle = new WikiArticle) {}
+
+class SetterArticleFilter(val wrappedPage: WrappedPage) extends IArticleFilter {
+  @throws(classOf[SAXException])
+  def process(page: WikiArticle, siteinfo: Siteinfo) {
+    wrappedPage.page = page
+  }
+}
 
 object WikiAnalytics {
 
-  def parseContentofPage(str: String): String = {
-    val xmlContent = scala.xml.XML.loadString(str)
-    val revision = xmlContent \\ "text"
-    revision.text
+  def parsePages(text: String): Option[(String, String)] = {
+
+    val wrappedPage = new WrappedPage
+    //The parser occasionally exceptions out, we ignore these
+    try {
+      val parser = new WikiXMLParser(new ByteArrayInputStream(text.getBytes), new SetterArticleFilter(wrappedPage))
+      parser.parse()
+    } catch {
+      case e: Exception =>
+    }
+    val page = wrappedPage.page
+    if (page.getText != null && page.getTitle != null
+      && page.getId != null && page.getRevisionId != null
+      && page.getTimeStamp != null) {
+      Some((page.getTitle, page.getText))
+    } else {
+      None
+    }
+
   }
+
 
   def runCluster(sc: SparkContext) {
     sc.hadoopConfiguration.set(XmlInputFormat.START_TAG_KEY, "<page>")
@@ -24,11 +57,11 @@ object WikiAnalytics {
       classOf[LongWritable],
       classOf[Text])
 
-    val rawContents = records.map(p => parseContentofPage(p._2.toString))
+    val rawContents = records.map(p => p._2.toString)
+    val plainText = rawContents.flatMap(parsePages)
 
-    rawContents.take(20).foreach(println)
+    plainText.take(30).foreach(println)
   }
-
 
 
 }
