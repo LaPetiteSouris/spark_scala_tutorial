5737a0b842fc89eb5d38ad9a0ee55284b6a450a5
LaPetiteSouris <sontunghoang@gmail.com>
2017-09-24 19:52:21 +0200

    Add document-wise-freq merge method

--- build.sbt
+++ build.sbt
@@ -9,11 +9,14 @@ libraryDependencies ++= {
   val sparkXMLVersion = "0.4.1"
   val cloud9Version = "2.0.1"
   val blikiVer = "3.1.0"
+  val stanfordnlp = "3.8.0"
   Seq(
     "org.apache.spark" %% "spark-core" % sparkVer % "provided" withSources(),
     "org.apache.spark" %% "spark-mllib" % sparkVer % "provided",
     "com.databricks" %% "spark-xml" % sparkXMLVersion,
-    "info.bliki.wiki" % "bliki-core" % blikiVer
+    "info.bliki.wiki" % "bliki-core" % blikiVer,
+    "edu.stanford.nlp" % "stanford-corenlp" % stanfordnlp
+
 
   )
 }
--- src/main/scala/WikiAnalytics.scala
+++ src/main/scala/WikiAnalytics.scala
@@ -1,6 +1,7 @@
 package main.scala
 
-import java.io.ByteArrayInputStream
+import java.io.{ByteArrayInputStream, FileOutputStream, PrintStream}
+import java.util.Properties
 
 import com.databricks.spark.xml.XmlInputFormat
 import org.apache.hadoop.io.{LongWritable, Text}
@@ -9,6 +10,13 @@ import com.databricks.spark.xml.XmlInputFormat
 import info.bliki.wiki.dump.{IArticleFilter, Siteinfo, WikiArticle, WikiXMLParser}
 import org.apache.spark.rdd.RDD
 import org.xml.sax.SAXException
+import edu.stanford.nlp.pipeline._
+import edu.stanford.nlp.ling.CoreAnnotations._
+import scala.collection.JavaConversions._
+import scala.collection.Map
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.ArrayBuffer
 
 
 case class Page(title: String, text: String, isCategory: Boolean, isFile: Boolean, isTemplate: Boolean)
@@ -22,7 +30,9 @@ class SetterArticleFilter(val wrappedPage: WrappedPage) extends IArticleFilter {
   }
 }
 
+
 object WikiAnalytics {
+
 
   def parsePages(text: String): Option[(String, String)] = {
 
@@ -45,11 +55,62 @@ object WikiAnalytics {
 
   }
 
+  def createNLPPipeline(): StanfordCoreNLP = {
+    val props = new Properties()
+    props.put("annotators", "tokenize, ssplit, pos, lemma")
+    new StanfordCoreNLP(props)
+  }
+
+  def plainTextToLemmas(text: String, stopWords: Set[String], pipeline: StanfordCoreNLP)
+  : Seq[String] = {
+    val doc = new Annotation(text)
+    pipeline.annotate(doc)
+    val lemmas = new ArrayBuffer[String]()
+    val sentences = doc.get(classOf[SentencesAnnotation])
+    for (sentence <- sentences; token <- sentence.get(classOf[TokensAnnotation])) {
+      val lemma = token.get(classOf[LemmaAnnotation])
+      if (lemma.length > 2 && !stopWords.contains(lemma) && isOnlyLetters(lemma)) {
+        lemmas += lemma.toLowerCase
+      }
+    }
+    lemmas
+  }
+
+  def isOnlyLetters(str: String): Boolean = {
+    // While loop for high performance
+    var i = 0
+    while (i < str.length) {
+      if (!Character.isLetter(str.charAt(i))) {
+        return false
+      }
+      i += 1
+    }
+    true
+  }
+
+  def termFrequency(term: Seq[String]): Map[String, Int] = {
+    term.groupBy(i => i).mapValues(_.size)
+  }
+
+
+  def merge(dfs: Map[String, Int], tfs: Map[String, Int]): Map[String, Int] = {
+    tfs.keySet.map(k => {
+      var count = 1
+      if (dfs.getOrElse(k, 0) != 0) {
+        count = dfs.getOrElse(k, 0) + 1
+      }
+      Map(k -> count)
+    }).reduceLeft(_ ++ _)
+    dfs ++ tfs
+  }
+
 
   def runCluster(sc: SparkContext) {
     sc.hadoopConfiguration.set(XmlInputFormat.START_TAG_KEY, "<page>")
     sc.hadoopConfiguration.set(XmlInputFormat.END_TAG_KEY, "</page>")
     sc.hadoopConfiguration.set(XmlInputFormat.ENCODING_KEY, "utf-8")
+    val stopWords = sc.broadcast(
+      scala.io.Source.fromFile("stopwords.txt").getLines().toSet).value
 
     val records = sc.newAPIHadoopFile(
       "hdfs:///user/dt/enwiki.xml",
@@ -60,7 +121,15 @@ object WikiAnalytics {
     val rawContents = records.map(p => p._2.toString)
     val plainText = rawContents.flatMap(parsePages)
 
-    plainText.take(30).foreach(println)
+    val lemmatized: RDD[Seq[String]] = plainText.mapPartitions(it => {
+      val pipeline = createNLPPipeline()
+      it.map { case (title, contents) =>
+        plainTextToLemmas(contents, stopWords, pipeline)
+      }
+    })
+    val docTermFreq: RDD[Map[String, Int]] = lemmatized.map(term => termFrequency(term))
+    docTermFreq.cache()
+    docTermFreq.take(30).foreach(println)
   }
 
 
